65cddb9b3c5634657107c2bb6263aa1acb0e0b31
Суздалев Станислав <suzdalev@kinoplan.tech>
2025-02-08 19:48:29 +0300

    Using EEPROM to save value and restore on next startup

--- Sketches/dimmer_delay/dimmer_delay.ino
+++ Sketches/dimmer_delay/dimmer_delay.ino
@@ -1,35 +1,51 @@
-int dimpin = 4;      // Выход диммера на симистор
+#include <EEPROM.h>
+
+unsigned int boot_count = 0;
+
+int boot_count_eeprom_addr = 0;
+int value_eeprom_addr = sizeof(boot_count);
 
 int min_value = 50;
 int max_value = 250;
-int value = min_value;       // Начальный уровень диммирования от 0 до 255
+int value = min_value; // Начальный уровень диммирования от 0 до 255
 
-int brightness_1 = 5;
-int brightness_2 = 6;
-int brightness_3 = 7;
-int brightness_4 = 8;
-int brightness_adjustable = 9;
+int dim_pin = 4; // Выход диммера на симистор
+
+int brightness_pin_1 = 5;
+int brightness_pin_2 = 6;
+int brightness_pin_3 = 7;
+int brightness_pin_4 = 8;
+int brightness_pin_adjustable = 9;
 
 bool should_increase = true;
-unsigned long timing;
+
+unsigned long brightness_timing;
+unsigned long eeprom_timing;
 
 void setup() {
-  pinMode(dimpin , OUTPUT);               // установим выход диммера
+  EEPROM.get(boot_count_eeprom_addr, boot_count);
+  EEPROM.get(value_eeprom_addr, value);
+
+  if (value < min_value) value = min_value;
+
+  EEPROM.put(0, boot_count++);
+
+  pinMode(dim_pin , OUTPUT);
   attachInterrupt(0, light, FALLING);     // прерывание для детектора нуля (2 пин!!!)
 
-  pinMode(brightness_1, INPUT_PULLUP);
-  pinMode(brightness_2, INPUT_PULLUP);
-  pinMode(brightness_3, INPUT_PULLUP);
-  pinMode(brightness_4, INPUT_PULLUP);
-  pinMode(brightness_adjustable, INPUT_PULLUP);
+  pinMode(brightness_pin_1, INPUT_PULLUP);
+  pinMode(brightness_pin_2, INPUT_PULLUP);
+  pinMode(brightness_pin_3, INPUT_PULLUP);
+  pinMode(brightness_pin_4, INPUT_PULLUP);
+  pinMode(brightness_pin_adjustable, INPUT_PULLUP);
 }
 
-void light() {                        // функция управления яркостью
-  if (value > 0 && value < 255) {         // утановим диммирование, если value не5 равен 0 или 255
-    delayMicroseconds(33*(255-value));  // вместо 33 былло 34, так между прочим. но если value = 5 лампа припадошно мигает
-    digitalWrite(dimpin , HIGH);
+void light() {
+  if (value > 0 && value < 255) {
+    delayMicroseconds(33*(255-value));    // вместо 33 былло 34, так между прочим. но если value = 5 лампа припадошно мигает
+    digitalWrite(dim_pin , HIGH);
     delayMicroseconds(500);
-    digitalWrite(dimpin , LOW);
+    digitalWrite(dim_pin , LOW);
   }
 }
 
@@ -38,15 +54,15 @@ int get_value_by_percent (int percent) {
 }
 
 void adjusting_mode() {
-  if (digitalRead(brightness_1) == LOW) {
+  if (digitalRead(brightness_pin_1) == LOW) {
     value = min_value;
-  } else if (digitalRead(brightness_2) == LOW) {
+  } else if (digitalRead(brightness_pin_2) == LOW) {
     value = get_value_by_percent(50);
-  } else if (digitalRead(brightness_3) == LOW) {
+  } else if (digitalRead(brightness_pin_3) == LOW) {
     value = get_value_by_percent(70);
-  } else if (digitalRead(brightness_4) == LOW) {
+  } else if (digitalRead(brightness_pin_4) == LOW) {
     value = max_value;
-  } else if (digitalRead(brightness_adjustable) == LOW && (millis() - timing >= 20)) {
+  } else if (digitalRead(brightness_pin_adjustable) == LOW && (millis() - brightness_timing >= 20)) {
     if (should_increase && value < max_value) {
       value += 1;
     } else if (!should_increase && value > min_value) {
@@ -55,11 +71,20 @@ void adjusting_mode() {
       should_increase = !should_increase;
     }
 
-    timing = millis();
+    brightness_timing = millis();
+  }
+}
+
+void update_eeprom_value() {
+  if (millis() - eeprom_timing >= 1000) {
+    EEPROM.put(value_eeprom_addr, value);
+
+    eeprom_timing = millis();
   }
 }
 
 void loop() {
   adjusting_mode();
+  update_eeprom_value();
 }
 
